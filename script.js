ec3c9a8ac130dfaf56f568717cacd517f9d488de
jyotirban borthakur <54855139+jyotirbanborthakur@users.noreply.github.com>
2021-01-20 12:49:47 +0530

    Add files via upload
    initial files

---
+++ script.js
@@ -0,0 +1,17 @@
+var firstName = 'mark';
+let markWeight = 78,
+    markHeight = 1.69;
+let johnWeight = 92,
+    johnHeight = 1.95;
+let markBmi = markWeight / markHeight ** 2;
+let johnBmi = johnWeight / johnHeight ** 2;
+
+console.log(markBmi, johnBmi);
+console.log(markBmi > johnBmi);
+
+const report = `i ${firstName} and my weight is ${markWeight} and my Bmi is ${markBmi}`;
+console.log(report);
+console.log(`daflk
+adfljnadf
+adfnad
+fafd`);
